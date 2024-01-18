e79a24b474bb12b9f174dc2d3c85ce0e81014b5d
km-backend08 <hassan.raza@kryptomind.com>
2024-01-18 18:35:42 +0500

    code

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/redirect", (req, res) => {
   // Redirect to the desired URL
   res.redirect(302, "https://www.example.com");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
